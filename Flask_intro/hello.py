d3e2ff4007eb4b76343028805fa50d1dc972fda7
SallyWang <sherryuuer@gmail.com>
2023-09-29 09:44:14 +0900

    20230929

--- Flask_intro/hello.py
+++ Flask_intro/hello.py
@@ -1,10 +1,16 @@
 from flask import Flask
 
 app = Flask(__name__)
+print(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run()
 
 # run on pycharm
 # now I understand the different between vscode and pycharm.Pycharm give me a env at the very first.
